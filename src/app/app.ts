2f384b698c4c4a8388602a84076dffa9bd3c32cb
Phong Tran Nguyen <phongtnguyen@kms-technology.com>
2026-02-07 20:47:38 +0700

    [CFS-11] Change project name

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('Confessly.Angular');
+  protected readonly title = signal('confessly.client');
 }
